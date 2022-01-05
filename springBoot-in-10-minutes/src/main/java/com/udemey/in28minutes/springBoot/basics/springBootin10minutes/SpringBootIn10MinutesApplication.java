0932dfeb6f7bef5ef3814870c4393cadea8683db
ANANYA <ANANYA@DESKTOP-UT16NAR>
2022-01-05 12:30:42 +0530

    initial commit

---
+++ springBoot-in-10-minutes/src/main/java/com/udemey/in28minutes/springBoot/basics/springBootin10minutes/SpringBootIn10MinutesApplication.java
@@ -0,0 +1,13 @@
+package com.udemey.in28minutes.springBoot.basics.springBootin10minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIn10MinutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIn10MinutesApplication.class, args);
+	}
+
+}
