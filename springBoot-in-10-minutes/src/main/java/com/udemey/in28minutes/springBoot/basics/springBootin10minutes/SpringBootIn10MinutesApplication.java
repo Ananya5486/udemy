af69f9fef39541844ca39ebbf910ae0361299546
ANANYA <ANANYA@DESKTOP-UT16NAR>
2022-01-05 15:27:21 +0530

    no message

--- springBoot-in-10-minutes/src/main/java/com/udemey/in28minutes/springBoot/basics/springBootin10minutes/SpringBootIn10MinutesApplication.java
+++ springBoot-in-10-minutes/src/main/java/com/udemey/in28minutes/springBoot/basics/springBootin10minutes/SpringBootIn10MinutesApplication.java
@@ -1,13 +1,18 @@
 package com.udemey.in28minutes.springBoot.basics.springBootin10minutes;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootIn10MinutesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootIn10MinutesApplication.class, args);
+		ApplicationContext applicationContext = (ApplicationContext) SpringApplication.run(SpringBootIn10MinutesApplication.class, args);
+		for (String name : applicationContext.get) {
+			
+		}
 	}
 
 }
