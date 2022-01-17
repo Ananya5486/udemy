2beb613b6307ea6b274eaba0216889bebb0e15e2
ANANYA <ANANYA@DESKTOP-UT16NAR>
2022-01-17 10:40:10 +0530

    Soap service for all courses

--- soap-course-management/src/main/java/com/udemy/in28minutes/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
+++ soap-course-management/src/main/java/com/udemy/in28minutes/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
@@ -1,5 +1,7 @@
 package com.udemy.in28minutes.soap.webservices.soapcoursemanagement.soap;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -7,6 +9,8 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import com.udemy.in28minutes.courses.CourseDetails;
+import com.udemy.in28minutes.courses.GetAllCourseDetailsRequest;
+import com.udemy.in28minutes.courses.GetAllCourseDetailsResponse;
 import com.udemy.in28minutes.courses.GetCourseDetailsRequest;
 import com.udemy.in28minutes.courses.GetCourseDetailsResponse;
 import com.udemy.in28minutes.soap.webservices.soapcoursemanagement.soap.bean.Course;
@@ -14,27 +18,51 @@ import com.udemy.in28minutes.soap.webservices.soapcoursemanagement.soap.service.
 
 @Endpoint
 public class CourseDetailsEndpoint {
-	
+
 	@Autowired
-	private CourseDetailsService service; 
-	//request-> GetCourseDetailsRequest
-	//Response-> GetCourseDetailsResponse
-	//namespace-> 
-	//name->GetCourseDetailsRequest
+	private CourseDetailsService service;
+
+	// request-> GetCourseDetailsRequest
+	// Response-> GetCourseDetailsResponse
+	// namespace->
+	// name->GetCourseDetailsRequest
 	@PayloadRoot(namespace = "http://in28minutes.udemy.com/courses", localPart = "GetCourseDetailsRequest")
 	@ResponsePayload
-	public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest courseDetailsRequest) {
+	public GetCourseDetailsResponse processCourseDetailsRequest(
+			@RequestPayload GetCourseDetailsRequest courseDetailsRequest) {
 		Course course = service.findById(courseDetailsRequest.getId());
-		return mapCourse(course);
+		return mapCourseDetails(course);
 	}
-	private GetCourseDetailsResponse mapCourse(Course course) {
-		GetCourseDetailsResponse response=new GetCourseDetailsResponse();
-		CourseDetails courseDetails=new CourseDetails();
+	@PayloadRoot(namespace = "http://in28minutes.udemy.com/courses", localPart = "GetAllCourseDetailsRequest")
+	@ResponsePayload
+	public GetAllCourseDetailsResponse processCourseDetailsRequest(
+			@RequestPayload GetAllCourseDetailsRequest courseDetailsRequest) {
+		List<Course> courses = service.findAll();
+		return mapAllCourseDetails(courses);
+	}
+	private GetCourseDetailsResponse mapCourseDetails(Course course) {
+		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		response.setCourseDetails(mapCourse(course));
+		return response;
+	}
+
+	private GetAllCourseDetailsResponse mapAllCourseDetails(List<Course> courses) {
+		GetAllCourseDetailsResponse response = new GetAllCourseDetailsResponse();
+		for (Course course : courses) {
+			CourseDetails mapCourse = mapCourse(course);
+			response.getCourseDetails().add(mapCourse);
+		}
+		return response;
+	}
+
+	private CourseDetails mapCourse(Course course) {
+		CourseDetails courseDetails = new CourseDetails();
 		courseDetails.setId(course.getId());
 		courseDetails.setName(course.getName());
 		courseDetails.setDescription(course.getDescription());
-		response.setCourseDetails(courseDetails);
-		return response;
+		return courseDetails;
 	}
+
+	
 
 }
