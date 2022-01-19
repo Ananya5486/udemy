e71354898fceb58437004ddbac1d6bdc2e4e5098
ANANYA <ANANYA@DESKTOP-UT16NAR>
2022-01-19 17:29:50 +0530

    hello world returning as a bean

---
+++ restful-web-services/src/main/java/com/udemy/in28minutes/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,27 @@
+package com.udemy.in28minutes.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public HelloWorldBean(String message) {
+		super();
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("HelloWorldBean [message=%s]",message);
+	}
+	
+	
+}
--- restful-web-services/src/main/java/com/udemy/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/udemy/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,20 +1,23 @@
 package com.udemy.in28minutes.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-	//GET
-	//URI->/helloWorld
-	//method "hello world 	 
-	
+	// GET
+	// URI->/helloWorld
+	// method "hello world
+
 //	@RequestMapping(method = RequestMethod.GET,path = "/helloWorld")
 	@GetMapping("/helloWorld")
 	public String helloWorld() {
 		return "Hello World";
+	}
+
+	@GetMapping("/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
 	}
 
 }
