76f37c6d0c93820778f33dbfef7f38736a178103
ANANYA <ANANYA@DESKTOP-UT16NAR>
2022-01-20 20:12:23 +0530

    Adding Post service to add the user

--- restful-web-services/src/main/java/com/udemy/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/udemy/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,5 +26,10 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User retrieveOneUser(@PathVariable int id) {
 		return daoService.findOne(id);
+	}
+	
+	@PostMapping("/users")
+	public void addUser(@RequestBody User user) {
+		User savedUser = daoService.save(user);
 	}
 }
