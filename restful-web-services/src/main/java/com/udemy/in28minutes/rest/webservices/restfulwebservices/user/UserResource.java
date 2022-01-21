b02518773106b143fbc85c9fa21143d43bde6c48
ANANYA <ANANYA@DESKTOP-UT16NAR>
2022-01-21 12:32:12 +0530

    Enhancing the post mapping with http status code

--- restful-web-services/src/main/java/com/udemy/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/udemy/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,35 +1,42 @@
 package com.udemy.in28minutes.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
 
 	@Autowired
 	private UserDaoService daoService;
-	//GET /users
-	//retrieveAllUser
-	
+	// GET /users
+	// retrieveAllUser
+
 	@GetMapping("/users")
-	public List<User> retrieveAllUser(){
+	public List<User> retrieveAllUser() {
 		return daoService.findAll();
 	}
-	//GET /users/{id}
-	//retrieveUser(int id) 
+
+	// GET /users/{id}
+	// retrieveUser(int id)
 	@GetMapping("/users/{id}")
 	public User retrieveOneUser(@PathVariable int id) {
 		return daoService.findOne(id);
 	}
-	
+
 	@PostMapping("/users")
-	public void addUser(@RequestBody User user) {
+	public ResponseEntity<Object> addUser(@RequestBody User user) {
 		User savedUser = daoService.save(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
