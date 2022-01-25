5ec9d0975a8f113976d27ede1bdffe11403c2973
ANANYA <ANANYA@DESKTOP-UT16NAR>
2022-01-25 11:59:28 +0530

    Internationalization update

--- restful-web-services/src/main/java/com/udemy/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/udemy/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
@@ -18,16 +19,9 @@ public class RestfulWebServicesApplication {
 
 	@Bean
 	LocaleResolver localeResolver() {
-		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver sessionLocaleResolver = new AcceptHeaderLocaleResolver();
 		sessionLocaleResolver.setDefaultLocale(Locale.US);
 		return sessionLocaleResolver;
-	}
-
-	@Bean
-	ResourceBundleMessageSource bundleMessageSource() {
-		ResourceBundleMessageSource bundleMessageSource = new ResourceBundleMessageSource();
-		bundleMessageSource.setBasename("messages");
-		return bundleMessageSource;
 	}
 
 }
